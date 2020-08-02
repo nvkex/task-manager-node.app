f8ee103a481fb88fdf124d274de6ecefc8f83ec2
nvkex <scrbwrm@gmail.com>
2020-08-02 12:42:46 +0530

    insertOne and insertMany

--- mongodb.js
+++ mongodb.js
@@ -8,18 +8,36 @@ const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 MongoClient.connect(
-    connectionURL, 
-    {useNewUrlParser: true},
+    connectionURL,
+    { useNewUrlParser: true, useUnifiedTopology:true },
     (error, client) => {
 
         // Handle errors
-        if(error){
+        if (error) {
             return console.log(error);
         }
         console.log('Connected!');
 
         const db = client.db(databaseName);
-        db.collection('users').insertOne({name: 'Jetha Lal', age: 44});
-        
+
+        // Insert a single document in the collection
+        // db.collection('users').insertOne(
+        //     {name: 'Jetha Lal', age: 44},
+        //     (err, result) => {
+        //         console.log(result.ops);
+        //     });
+
+        // Insert multiple documents in the colllection
+        db.collection('tasks').insertMany(
+            [
+                { description: 'NodeJS', progress: 70, completed: false },
+                { description: 'ReactJS', progress: 60,completed: false },
+                { description: 'VueJS',  progress: 30,ompleted: false }
+            ],
+            (err, result) => {
+                console.log(result.ops);
+            }
+        )
+
 
     })
