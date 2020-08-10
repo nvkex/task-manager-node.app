3288719fc75cfccc748ca32c0035f61182df9578
nvkex <scrbwrm@gmail.com>
2020-08-10 22:47:06 +0530

    use default JSOn Parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+// Set default Port
 const port = process.env.PORT || 3001;
+
+// JSON Parser 
+app.use(express.json());
 
 app.post('/users', (req,res) => {
     res.send('Working');
